2c20cb66f8925a00440283adc77f9bb9ec37613d
Milana <bigaeva03@inbox.ru>
2026-03-06 15:27:22 +0300

    refactor(main): замена AutoMigrate на golang-migrate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,14 +7,39 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/lab2/rest-api/internal/config"
-	"github.com/lab2/rest-api/internal/domain"
+
+	//"github.com/lab2/rest-api/internal/domain"
 	"github.com/lab2/rest-api/internal/handler"
 	"github.com/lab2/rest-api/internal/middleware"
 	"github.com/lab2/rest-api/internal/repository"
 	"github.com/lab2/rest-api/internal/service"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	// Библиотека миграций
+	// Сам пакет migrate
+	"github.com/golang-migrate/migrate/v4"
+	// Драйверы импортируем с подчеркиванием _, чтобы они не конфликтовали с gorm.io/driver/postgres
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
+
+// runMigrations запускает миграции базы данных
+func runMigrations(dsn string) error {
+	m, err := migrate.New(
+		"file://internal/migrations",
+		dsn,
+	)
+	if err != nil {
+		return fmt.Errorf("ошибка инициализации миграций: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("ошибка применения миграций: %w", err)
+	}
+
+	return nil
+}
 
 func main() {
 	cfg, err := config.Load()
@@ -27,10 +52,17 @@ func main() {
 		log.Fatalf("connect db: %v", err)
 	}
 
-	if err := db.AutoMigrate(&domain.Category{}, &domain.Product{}); err != nil {
-		log.Fatalf("migrate: %v", err)
+	// Запускаем миграции перед стартом приложения
+	if err := runMigrations(cfg.DSN()); err != nil {
+		log.Fatalf("Ошибка миграций: %v", err)
 	}
+	log.Println("Миграции успешно применены")
 
+	/*
+		if err := db.AutoMigrate(&domain.Category{}, &domain.Product{}); err != nil {
+			log.Fatalf("migrate: %v", err)
+		}
+	*/
 	categoryRepo := repository.NewCategoryRepository(db)
 	productRepo := repository.NewProductRepository(db)
 	categorySvc := service.NewCategoryService(categoryRepo, productRepo)
